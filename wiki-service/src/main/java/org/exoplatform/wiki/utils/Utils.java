73b10e7b47274741ae7b32b8e77c37072642c00b
hungphongbk <hungphongbk@gmail.com>
2012-12-11 09:16:43 +0700

    WIKI-113 Fix wrong default permission of wiki

--- wiki-service/src/main/java/org/exoplatform/wiki/utils/Utils.java
+++ wiki-service/src/main/java/org/exoplatform/wiki/utils/Utils.java
@@ -17,6 +17,7 @@ import javax.jcr.query.QueryResult;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 
+import org.apache.commons.lang.StringUtils;
 import org.chromattic.core.api.ChromatticSessionImpl;
 import org.exoplatform.container.ExoContainer;
 import org.exoplatform.container.ExoContainerContext;
@@ -525,7 +526,9 @@ public class Utils {
     UserACL userACL = (UserACL) ExoContainerContext.getCurrentContainer().getComponentInstanceOfType(UserACL.class);
     permissionMap.put(userACL.getSuperUser(), IDType.USER);
     for (String group : userACL.getPortalCreatorGroups()) {
-      permissionMap.put(group, IDType.MEMBERSHIP);
+      if (!StringUtils.isEmpty(group)) {
+        permissionMap.put(group, IDType.MEMBERSHIP);
+      }
     }
     return permissionMap;
   }
