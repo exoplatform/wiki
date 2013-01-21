34839a2495105cd06d9e6c1d25740af5116e76d8
haquangtan <tanhq@exoplatform.com>
2013-01-21 18:47:14 +0700

    Fix js problems after merging

--- wiki-webapp/src/main/webapp/javascript/eXo/wiki/UIWikiSearchBox.js
+++ wiki-webapp/src/main/webapp/javascript/eXo/wiki/UIWikiSearchBox.js
@@ -15,8 +15,7 @@
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- */
- (function(base, uiForm, webuiExt, $) {
+ */(function(base, uiForm, webuiExt, $) {
 
 function UIWikiSearchBox() {
   this.restURL = null;
