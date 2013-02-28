b6238b574c2c5bb8fbdae6c2639618a1b68f3ada
hungphongbk <hungphongbk@gmail.com>
2013-02-28 17:08:28 +0700

    WIKI-267 Related Page Panel is incorrect

--- wiki-webapp/src/main/webapp/javascript/eXo/wiki/related/UIRelated.js
+++ wiki-webapp/src/main/webapp/javascript/eXo/wiki/related/UIRelated.js
@@ -13,43 +13,39 @@ UIRelated.prototype.initMacros = function() {
   if (editForm != null) {
     var ifm = $(editForm).find('iframe.gwt-RichTextArea')[0];
     if (ifm != null) {
-    var innerDoc = ifm.contentDocument || ifm.contentWindow.document;
-    relatedBlocks = $.merge(relatedBlocks, $(innerDoc).find(
-        ".RelatedMacro"));
+      var innerDoc = ifm.contentDocument || ifm.contentWindow.document;
+      relatedBlocks = $.merge(relatedBlocks, $(innerDoc).find(".RelatedMacro"));
     }
   }
+  
   for ( var i = 0; i < relatedBlocks.length; i++) {
     var relatedBlock = relatedBlocks[i];
     var infoElement = $(relatedBlock).find('input.info')[0];
     var restUrl = infoElement.getAttribute("restUrl");
     var redirectTempl = infoElement.getAttribute("redirectUrl");
-    if ($(relatedBlock).find("div.TreeNodeType").length > 0)
+    if ($(relatedBlock).find("div.TreeNodeType").length > 0) {
       return;
+    }
+      
     $.ajax({
       async : false,
       url : restUrl,
       type : 'GET',
       data : '',
       success : function(data) {
+        $(relatedBlock).empty();
         for ( var i = 0; i < data.jsonList.length; i++) {
           var relatedItem = data.jsonList[i];
-          $(relatedBlock).append($('<div/>', {
-            'class' : 'Page TreeNodeType Node'
-            }).append($('<div/>')
-               .append($('<div/>', {
-                 'class' : 'NodeLabel'
-               }).append( $('<a/>', {
-                  title : relatedItem.title,
-                  href : redirectTempl + "&objectId=" + encodeURIComponent(relatedItem.identity),
-                  text: relatedItem.title
-                  }))
-                )
-              )
-            )
-          }
+          var href = redirectTempl + "&objectId=" + encodeURIComponent(relatedItem.identity);
+          $(relatedBlock).append("<a href='" + href + "' class='relateItem'><i class='uiIconFile'></i>" + relatedItem.title + "</a>");
         }
+        
+        // Recalculate width height
+        if (eXo.wiki.WikiLayout) {
+	      eXo.wiki.WikiLayout.processWithHeight();
+	    }
       }
-    );
+    });
   }
 };
 
