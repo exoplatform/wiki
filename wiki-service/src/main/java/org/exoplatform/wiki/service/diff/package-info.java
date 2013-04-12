ed7155b25707b15ba71cb9f6c9fc1629b430e398
Franck Bassy <franck.bassy@gmail.com>
2013-04-12 16:12:01 +0700

    WIKI-465 Java API - Improve the javadoc : add a package description

--- wiki-service/src/main/java/org/exoplatform/wiki/service/diff/package-info.java
+++ wiki-service/src/main/java/org/exoplatform/wiki/service/diff/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Provide classes to be able to compare and make a diff list and results
+ * Provides classes to be able to compare and make a diff list and results
  * between content.
  */
 package org.exoplatform.wiki.service.diff;
