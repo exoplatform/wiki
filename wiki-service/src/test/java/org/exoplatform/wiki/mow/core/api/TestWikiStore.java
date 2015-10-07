51824bdec6b2bc1bfb892cb0b7ca19770ee63fca
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2015-10-07 23:15:52 +0200

    Remove useless test

--- wiki-service/src/test/java/org/exoplatform/wiki/mow/core/api/TestWikiStore.java
+++ wiki-service/src/test/java/org/exoplatform/wiki/mow/core/api/TestWikiStore.java
@@ -16,7 +16,9 @@
  */
 package org.exoplatform.wiki.mow.core.api;
 
+import org.exoplatform.portal.config.model.PortalConfig;
 import org.exoplatform.wiki.WikiException;
+import org.exoplatform.wiki.mow.api.Wiki;
 import org.exoplatform.wiki.mow.api.WikiType;
 import org.exoplatform.wiki.mow.core.api.wiki.*;
 
@@ -70,46 +72,6 @@ public class TestWikiStore extends AbstractMOWTestcase {
     PortalWiki wiki = portalWikiContainer.addWiki("classic");
     PageImpl wikiHomePage = wiki.getWikiHome();
     assertNotNull(wikiHomePage);
-  }
-  
-  public void testAddAndGetPortalClassicWikiPage() throws WikiException {
-    WikiStoreImpl wStore = (WikiStoreImpl) mowService.getWikiStore();
-    WikiContainer<PortalWiki> portalWikiContainer = wStore.getWikiContainer(WikiType.PORTAL);
-    PortalWiki wiki = portalWikiContainer.addWiki("classic");
-    assertNotNull(wiki) ;
-    /*WikiHome wikiHomePage = wiki.getWikiHome();
-    AttachmentImpl content = wiki.createContent() ;
-    assertNotNull(content) ;
-    
-    wikiHomePage.setContent(content) ;    
-    
-    content.setSyntax("xwiki_2.0");
-    content.setText("This is exo wiki") ;
-    
-    AttachmentImpl addedContent = wikiHomePage.getContent() ;
-    
-    assertNotNull(addedContent) ;
-    assertEquals(addedContent.getSyntax(), "xwiki_2.0") ;
-    assertEquals(addedContent.getText(), "This is exo wiki") ;
-    
-    PageImpl wikipage = wiki.createWikiPage();
-    wikipage.setName("Hello World Wiki Page"); 
-    
-    wikiHomePage.addWikiPage(wikipage);
-    assertSame(wikipage, wikiHomePage.getChildPages().iterator().next());
-    PageImpl wikiChildPage = wiki.createWikiPage();
-    wikiChildPage.setName("Hello World Wiki  Child Page");
-    wikiChildPage.setParentPage(wikipage);
-    assertSame(wikiChildPage, wikipage.getChildPages().iterator().next());
-    
-    AttachmentImpl pageContent = wiki.createContent() ;
-    wikipage.setContent(pageContent) ;
-    pageContent.setSyntax("exowiki_2.0") ;
-    pageContent.setText("This is the first page's content") ;
-    AttachmentImpl addedPageContent = wikipage.getContent() ;
-    assertEquals(addedPageContent.getSyntax(), "exowiki_2.0") ;
-    assertEquals(addedPageContent.getText(), "This is the first page's content") ;
-    */
   }
 
 }
