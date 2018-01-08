0f73208ab80aa9597a3db492b91057ee437b9471
Boubaker Khanfir <bkhanfir@exoplatform.com>
2018-01-08 15:23:25 +0100

    SOC-5962 Update MockSettingService Impl (#139)

--- wiki-jpa-dao/src/test/java/org/exoplatform/wiki/jpa/mock/MockSettingService.java
+++ wiki-jpa-dao/src/test/java/org/exoplatform/wiki/jpa/mock/MockSettingService.java
@@ -86,11 +86,12 @@ public class MockSettingService implements SettingService {
     }
 
     @Override
-    public Set<String> getEmptyContextsByScopeAndContextType(String contextType,
-                                                             String scopeType,
-                                                             String scopeName,
-                                                             int offset,
-                                                             int limit) {
+    public Set<String> getEmptyContextsByTypeAndScopeAndSettingName(String contextType,
+                                                                    String scopeType,
+                                                                    String scopeName,
+                                                                    String settingName,
+                                                                    int offset,
+                                                                    int limit) {
       return null;
     }
 
