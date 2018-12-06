855c8886ada1291cfe290c4bc57bdf19639957a2
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2018-12-06 15:02:22 +0100

    WIKI-1439 : add new Vue component for children pages feature (WIP)

---
+++ wiki-webapp/src/main/webapp/javascript/eXo/wiki/ckeditor/plugins/ExoWikiChildrenPages.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wiki-children-pages">
+    <ul class="childrenPages">
+      <li v-for="label in childrenPages"><a href="#">{{ label }}</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { getUserAvatar, getSpaceAvatar, getUserProfileLink, getSpaceProfileLink, escapeHtml } from '../chatServices';
+import {chatConstants} from '../chatConstants';
+
+export default {
+  props: {
+    /** Contact display name */
+    depth: {
+      type: Number,
+      default: 1
+    }
+  },
+  data : function() {
+    return {
+    };
+  },
+  computed: {
+    childrenPages: function() {
+      return ['Page 10', 'Page 11'];
+    }
+  },
+  created() {
+  },
+  methods: {
+  }
+};
+</script>
