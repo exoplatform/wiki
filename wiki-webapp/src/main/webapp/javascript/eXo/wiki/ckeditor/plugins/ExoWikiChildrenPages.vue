31dc93a3b5584aaf8c3714fac1b7265c9b8101b5
alaameddeb <aemeddeb@exoplatform.com>
2018-12-11 17:56:07 +0100

    WIKI-1439 : implement ExoWikiChildrenPages component

--- wiki-webapp/src/main/webapp/javascript/eXo/wiki/ckeditor/plugins/ExoWikiChildrenPages.vue
+++ wiki-webapp/src/main/webapp/javascript/eXo/wiki/ckeditor/plugins/ExoWikiChildrenPages.vue
@@ -1,19 +1,17 @@
 <template>
-  <div class="wiki-children-pages">
-    <ul class="childrenPages">
-      <li v-for="label in childrenPages" :key="label">
-        <a href="#">
-          {{ label }}
-        </a>
-      </li>
-    </ul>
-  </div>
+  <ul class="wiki-children-pages">
+    <li v-for="page in childrenPages" :key="page.name">
+      <a :href="getChildrenPagePath(page.name)">
+        {{ page.name }}
+      </a>
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
   props: {
-    /** Contact display name */
+    /** Number of page depth */
     depth: {
       type: Number,
       default: 1
@@ -21,14 +19,33 @@ export default {
   },
   data : function() {
     return {
+      childrenPages: []
     };
   },
-  computed: {
-    childrenPages: function() {
-      return ['Page 10', 'Page 11'];
-    }
+  created() {
+    this.getChildrenPages();
   },
   methods: {
+    getChildrenPages() {
+      const pageName = eXo.env.server.portalBaseURL.substr(eXo.env.server.portalBaseURL.lastIndexOf('/') + 1);
+      let url = '';
+      if(eXo.env.portal.spaceName) {
+        url = `/rest/wiki/tree/CHILDREN?path=group/spaces/${eXo.env.portal.spaceGroup}/${pageName}&depth=${this.depth}`;
+      } else {
+        url = `/rest/wiki/tree/CHILDREN?path=portal/${eXo.env.portal.portalName}/${pageName}&depth=${this.depth}`;
+      }
+      fetch(url, {credentials: 'include'}).then(resp => resp.json()).then(data => {
+        if (data && data.jsonList) {
+          this.childrenPages = data.jsonList;
+        } 
+      });
+    },
+    getChildrenPagePath(pageName) {
+      if (pageName) {
+        return pageName.split(' ').join('_');
+      }
+      return '#';
+    }
   }
 };
 </script>
