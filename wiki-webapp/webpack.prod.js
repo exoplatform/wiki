570fbd0e250661664c2263e3937dfbbed135b6b8
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2019-04-08 15:38:34 +0200

    WIKI-1439 : improve webpack conf

--- wiki-webapp/webpack.prod.js
+++ wiki-webapp/webpack.prod.js
@@ -4,10 +4,7 @@ const webpackCommonConfig = require('./webpack.common.js');
 
 let config = merge(webpackCommonConfig, {
   output: {
-    path: path.resolve(__dirname, './target/wiki/javascript/eXo/wiki/ckeditor'),
-    filename: '[name].bundle.js',
-    library: '[name]',
-    libraryTarget: 'amd'
+    path: path.resolve(__dirname, './target/wiki/javascript/eXo/wiki/ckeditor')
   }
 });
 
