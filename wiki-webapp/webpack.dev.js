b8dea72112f5fe1f395ed63df54b7a25eaf8b686
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2019-04-25 09:12:12 +0200

    WIKI-1439 : Include page plugin - first step : display contextual popup with input field

--- wiki-webapp/webpack.dev.js
+++ wiki-webapp/webpack.dev.js
@@ -3,7 +3,7 @@ const merge = require('webpack-merge');
 const webpackCommonConfig = require('./webpack.common.js');
 
 // change the server path to your server location path
-const exoServerPath = "/home/thomas/exoplatform/bundles/plf-community-tomcat-standalone-5.3.x-wiki-editor-20190408.121601-1/platform-community-5.3.x-wiki-editor-SNAPSHOT/";
+const exoServerPath = "/home/thomas/exoplatform/bundles/plfent-5.3.x-wiki-editor-20190409.124152-7/platform-5.3.x-wiki-editor-SNAPSHOT/";
 
 let config = merge(webpackCommonConfig, {
   mode: 'development',
@@ -12,7 +12,7 @@ let config = merge(webpackCommonConfig, {
   },
 
   // Useful for debugging
-  devtool: 'eval-source-map'
+  devtool: 'cheap-eval-source-map'
 });
 
 
