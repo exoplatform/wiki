f98c5154af86288ecad68cacb3390d934893a1dc
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2019-05-14 09:45:35 +0200

    WIKI-1408: Users can remove attached files on wiki page even if they don't have edit permission (#194)
    * WIKI-1408: Users can remove attached files on wiki page even if they don't have edit permission

--- wiki-webui/src/main/java/org/exoplatform/wiki/webui/control/filter/RemoveAttachmentPermissionFilter.java
+++ wiki-webui/src/main/java/org/exoplatform/wiki/webui/control/filter/RemoveAttachmentPermissionFilter.java
@@ -1,12 +1,22 @@
 package org.exoplatform.wiki.webui.control.filter;
 
+import org.exoplatform.commons.utils.CommonsUtils;
+import org.exoplatform.container.ExoContainerContext;
+import org.exoplatform.services.security.ConversationState;
 import org.exoplatform.webui.ext.filter.UIExtensionAbstractFilter;
 import org.exoplatform.webui.ext.filter.UIExtensionFilterType;
+import org.exoplatform.wiki.commons.Utils;
+import org.exoplatform.wiki.mow.api.Page;
+import org.exoplatform.wiki.mow.api.PermissionType;
+import org.exoplatform.wiki.service.WikiPageParams;
+import org.exoplatform.wiki.service.WikiService;
 
 import java.util.Map;
 
 public class RemoveAttachmentPermissionFilter extends UIExtensionAbstractFilter {
   public static final String ATTACHMENT_KEY = "attachmentName";
+
+  private WikiService wikiService;
 
   public RemoveAttachmentPermissionFilter() {
     this(null);
@@ -14,12 +24,33 @@ public class RemoveAttachmentPermissionFilter extends UIExtensionAbstractFilter 
 
   public RemoveAttachmentPermissionFilter(String messageKey) {
     super(messageKey, UIExtensionFilterType.MANDATORY);
+    this.wikiService = CommonsUtils.getService(WikiService.class);
+
   }
 
   @Override
   public boolean accept(Map<String, Object> context) throws Exception {
-    // TODO there is no permissions on attachments, why do we need a filter ?
-    return true;
+    WikiPageParams pageParams = Utils.getCurrentWikiPageParams();
+    if (pageParams == null) {
+      return false;
+    }
+
+    if (pageParams.getOwner() == null || pageParams.getPageName() == null || pageParams.getType() == null) {
+      return false;
+    }
+    Page page = wikiService.getPageOfWikiByName(pageParams.getType(), pageParams.getOwner(), pageParams.getPageName());
+    if (page == null) {
+      return false;
+    }
+
+    if (ConversationState.getCurrent() == null) {
+      throw new IllegalStateException("No Identity found: ConversationState.getCurrent() is null");
+    }
+    if (ConversationState.getCurrent().getIdentity()==null) {
+      throw new IllegalStateException("No Identity found: ConversationState.getCurrent().getIdentity() is null");
+    }
+    return wikiService.hasPermissionOnPage(page, PermissionType.EDITPAGE, ConversationState.getCurrent().getIdentity());
+
   }
 
   @Override
