4eb9be679435c419d252d30a343a03c5142195c7
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2020-05-11 15:26:13 +0200

    Migrate all pages

--- wiki-service/src/main/java/org/exoplatform/wiki/upgrade/PageContentMigrationService.java
+++ wiki-service/src/main/java/org/exoplatform/wiki/upgrade/PageContentMigrationService.java
@@ -1,8 +1,13 @@
 package org.exoplatform.wiki.upgrade;
 
+import org.exoplatform.commons.persistence.impl.EntityManagerService;
+import org.exoplatform.container.ExoContainerContext;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 import org.exoplatform.wiki.WikiException;
+import org.exoplatform.wiki.jpa.EntityConverter;
+import org.exoplatform.wiki.jpa.dao.PageDAO;
+import org.exoplatform.wiki.jpa.entity.PageEntity;
 import org.exoplatform.wiki.mow.api.Page;
 import org.exoplatform.wiki.rendering.RenderingService;
 import org.exoplatform.wiki.service.PageUpdateType;
@@ -10,6 +15,9 @@ import org.exoplatform.wiki.service.WikiService;
 import org.xwiki.component.manager.ComponentLookupException;
 import org.xwiki.rendering.converter.ConversionException;
 import org.xwiki.rendering.syntax.Syntax;
+
+import javax.persistence.EntityManager;
+import java.util.List;
 
 public class PageContentMigrationService {
 
@@ -19,9 +27,15 @@ public class PageContentMigrationService {
 
   private WikiService wikiService;
 
-  public PageContentMigrationService(RenderingService renderingService, WikiService wikiService) {
+  private EntityManagerService entityManagerService;
+
+  private PageDAO pageDAO;
+
+  public PageContentMigrationService(RenderingService renderingService, WikiService wikiService, EntityManagerService entityManagerService, PageDAO pageDAO) {
     this.renderingService = renderingService;
     this.wikiService = wikiService;
+    this.entityManagerService = entityManagerService;
+    this.pageDAO = pageDAO;
   }
 
   /**
@@ -31,14 +45,69 @@ public class PageContentMigrationService {
    * @throws ConversionException
    * @throws WikiException
    */
-  public void migratePage(Page page) throws ComponentLookupException, ConversionException, WikiException {
+  public void migratePage(Page page) throws Exception {
     if(Syntax.XWIKI_2_0.toIdString().equals(page.getSyntax())) {
       LOG.info("Convert wiki page " + page.getId() + " to HTML");
       String markup = page.getContent();
       markup = renderingService.render(markup, page.getSyntax(), Syntax.XHTML_1_0.toIdString(), false);
       page.setContent(markup);
       page.setSyntax(Syntax.XHTML_1_0.toIdString());
-      wikiService.updatePage(page, PageUpdateType.EDIT_PAGE_CONTENT);
+      wikiService.updatePage(page, null);
+    }
+  }
+
+  /**
+   * Migrate all pages from XWiki 2.0 syntax to HTML
+   */
+  public void migrateAllPages() {
+    int offset = 0;
+    int batchSize = 100;
+
+    int nbMigratedPages = 0;
+    int nbMigrationErrors = 0;
+
+    try {
+      entityManagerService.startRequest(ExoContainerContext.getCurrentContainer());
+
+      Long nbPagesToMigrate = pageDAO.countPagesBySyntax(Syntax.XWIKI_2_0.toIdString());
+      if (nbPagesToMigrate == null || nbPagesToMigrate == 0) {
+        LOG.info("Wiki page syntax migration - No Wiki page to migrate from XWiki syntax to HTML");
+        return;
+      }
+
+      LOG.info("== Starting Wiki page syntax migration");
+      LOG.info("Wiki page syntax migration - Number of pages = " + nbPagesToMigrate);
+
+      List<PageEntity> pagesToMigrate;
+      do {
+        entityManagerService.endRequest(ExoContainerContext.getCurrentContainer());
+        entityManagerService.startRequest(ExoContainerContext.getCurrentContainer());
+
+        pagesToMigrate = pageDAO.findAllBySyntax(Syntax.XWIKI_2_0.toIdString(), offset, batchSize);
+        for (PageEntity pageEntity : pagesToMigrate) {
+          Page page = null;
+          try {
+            page = EntityConverter.convertPageEntityToPage(pageEntity);
+            migratePage(page);
+            nbMigratedPages++;
+          } catch (Exception e) {
+            LOG.error("Error while migrating wiki page " + (page != null ? page.getId() : "") + " to HTML", e);
+            nbMigrationErrors++;
+            pageEntity.setSyntax("ERROR");
+            pageDAO.update(pageEntity);
+          }
+        }
+
+        LOG.info("Wiki page syntax migration - Progress : " + nbMigratedPages + "/" + nbPagesToMigrate);
+
+        offset += batchSize;
+      } while (pagesToMigrate != null && !pagesToMigrate.isEmpty());
+
+      LOG.info("== Wiki page syntax migration - Migration finished - Number of migrated pages = " + nbMigratedPages + ", number of errors = " + nbMigrationErrors);
+    } catch (Exception e) {
+      LOG.error("Error while migrating wiki pages fom XWiki syntax to HTML", e);
+    } finally {
+      entityManagerService.endRequest(ExoContainerContext.getCurrentContainer());
     }
   }
 }
--- wiki-service/src/main/java/org/exoplatform/wiki/upgrade/WikiPageContentMigrationUpgradePlugin.java
+++ wiki-service/src/main/java/org/exoplatform/wiki/upgrade/WikiPageContentMigrationUpgradePlugin.java
@@ -2,16 +2,44 @@ package org.exoplatform.wiki.upgrade;
 
 import org.exoplatform.commons.api.settings.SettingService;
 import org.exoplatform.commons.upgrade.UpgradeProductPlugin;
+import org.exoplatform.container.ExoContainer;
+import org.exoplatform.container.ExoContainerContext;
 import org.exoplatform.container.xml.InitParams;
+import org.picocontainer.Startable;
 
-public class WikiPageContentMigrationUpgradePlugin extends UpgradeProductPlugin {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+public class WikiPageContentMigrationUpgradePlugin implements Startable {
+//public class WikiPageContentMigrationUpgradePlugin extends UpgradeProductPlugin {
+
+  private PageContentMigrationService migrationService;
 
   public WikiPageContentMigrationUpgradePlugin(SettingService settingService, InitParams initParams) {
-    super(settingService, initParams);
+    //super(settingService, initParams);
+    migrationService = ExoContainerContext.getService(PageContentMigrationService.class);
+  }
+
+  /*
+  @Override
+  public void processUpgrade(String newVersion, String previousVersion) {
+    migrationService.migrateAllPages();
+  }
+  */
+
+  @Override
+  public void start() {
+    ExoContainer container = ExoContainerContext.getCurrentContainer();
+    ExecutorService executorService = Executors.newSingleThreadExecutor(r -> new Thread(r, "Wiki-PageSyntaxMigration"));
+    executorService.execute(() -> {
+      ExoContainerContext.setCurrentContainer(container);
+      migrationService.migrateAllPages();
+    });
   }
 
   @Override
-  public void processUpgrade(String newVersion, String previousVersion) {
+  public void stop() {
 
   }
 }
