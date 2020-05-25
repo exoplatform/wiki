1fec9bb787525b9ed59ab20dda2cec08e567ba45
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2020-05-25 12:21:21 +0200

    Add back Include Page plugin

--- wiki-webapp/src/main/webapp/javascript/eXo/wiki/ckeditor/plugins/ExoWikiPage.vue
+++ wiki-webapp/src/main/webapp/javascript/eXo/wiki/ckeditor/plugins/ExoWikiPage.vue
@@ -5,7 +5,6 @@
 <script>
 export default {
   props: {
-    /** Included page name  */
     pageName: {
       type: String,
       default() {
@@ -18,14 +17,20 @@ export default {
       pageContent: ''
     };
   },
-  created() {
+  mounted() {
     this.getPageContent();
   },
   methods: {
     getPageContent() {
       if (this.pageName) {
-        // TODO api call to get page content using pageName
-        this.pageContent = `<h1>Content of page ${this.pageName}</h1>`;
+        const self = this;
+        const url = `/portal/rest/wiki/group/spaces//spaces/${eXo.env.portal.spaceGroup}/pages/${this.pageName}`;
+        fetch(url)
+          .then(response => response.text())
+          .then(text => (new window.DOMParser()).parseFromString(text, 'text/xml'))
+          .then(xml => xml.documentElement.getElementsByTagName('content')[0].childNodes[0].nodeValue)
+          .then(data => self.pageContent = data)
+          .catch(error => console.error(`Error while fetching content of page ${self.pageName}`, error));
       }
     }
   }
