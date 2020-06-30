06d7bfbbc296e76e8acc940ac08d896376bbf635
Ali HAMDI <ahamdi@exoplatform.com>
2020-06-30 09:33:45 +0100

    MAINT-25804 : Fix problem loading right wiki url for tree pages

--- wiki-webui/src/test/java/org/exoplatform/wiki/TestUtils.java
+++ wiki-webui/src/test/java/org/exoplatform/wiki/TestUtils.java
@@ -17,9 +17,70 @@
 package org.exoplatform.wiki;
 
 import junit.framework.TestCase;
+import org.apache.commons.lang.StringUtils;
+import org.exoplatform.commons.utils.CommonsUtils;
+import org.exoplatform.social.core.space.SpaceApplication;
+import org.exoplatform.social.core.space.SpaceTemplate;
+import org.exoplatform.social.core.space.model.Space;
+import org.exoplatform.social.core.space.spi.SpaceService;
+import org.exoplatform.social.core.space.spi.SpaceTemplateService;
+import org.exoplatform.wiki.commons.Utils;
+import org.exoplatform.wiki.service.WikiPageParams;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({CommonsUtils.class, org.exoplatform.social.webui.Utils.class})
 public class TestUtils extends TestCase {
 
   public void testGetURLFromParams() throws Exception {
+    // Mocks
+    SpaceService spaceService = Mockito.mock(SpaceService.class);
+    SpaceTemplateService spaceTemplateService = Mockito.mock(SpaceTemplateService.class);
+    PowerMockito.mockStatic(CommonsUtils.class);
+    PowerMockito.mockStatic(org.exoplatform.social.webui.Utils.class);
+
+    // Returned Objects from mocks
+    List<SpaceApplication> spaceApplicationList = new ArrayList<>();
+    SpaceApplication spaceApplication1 = new SpaceApplication();
+    spaceApplication1.setPortletName("WikiPortlet");
+    spaceApplication1.setUri("wiki");
+    spaceApplicationList.add(spaceApplication1);
+    SpaceApplication spaceApplication2 = new SpaceApplication();
+    spaceApplication2.setPortletName("SpaceSettingPortlet");
+    spaceApplication2.setUri("settings");
+    spaceApplicationList.add(spaceApplication2);
+    SpaceTemplate spaceTemplate = new SpaceTemplate();
+    spaceTemplate.setName("community");
+    spaceTemplate.setSpaceApplicationList(spaceApplicationList);
+    Space space = new Space();
+    space.setGroupId("/spaces/spaceTest");
+    space.setTemplate(spaceTemplate.getName());
+
+    // When
+    when(CommonsUtils.getService(SpaceService.class)).thenReturn(spaceService);
+    when(CommonsUtils.getService(SpaceTemplateService.class)).thenReturn(spaceTemplateService);
+    when(org.exoplatform.social.webui.Utils.getSpaceHomeURL(space)).thenReturn("/portal/g/:spaces:spaceTest/spaceTest");
+    when(spaceService.getSpaceByGroupId(Matchers.eq("/spaces/spaceTest"))).thenReturn(space);
+    when(spaceTemplateService.getSpaceTemplateByName(Matchers.eq("community"))).thenReturn(spaceTemplate);
+
+    // Then
+    WikiPageParams params = new WikiPageParams();
+    params.setOwner("");
+    assertEquals(Utils.getURLFromParams(params), StringUtils.EMPTY);
+    params.setOwner("/spaces/spaceTest");
+    params.setType("");
+    assertEquals(Utils.getURLFromParams(params), StringUtils.EMPTY);
+    params.setType("group");
+    assertEquals(Utils.getURLFromParams(params), "/portal/g/:spaces:spaceTest/spaceTest/wiki/");
   }
 }
