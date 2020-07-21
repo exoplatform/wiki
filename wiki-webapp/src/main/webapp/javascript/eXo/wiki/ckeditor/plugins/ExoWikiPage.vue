0b318ce81373fe34ed520dce6bc9dd0b5a2e17fc
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2020-07-21 10:47:04 +0100

    MAINT-26512 : Include page in the new Ckeditor does not work (#245) (#224)
    (cherry picked from commit 3a530ede8f0e755ac074cafee6ec140c3b7cbc67)

--- wiki-webapp/src/main/webapp/javascript/eXo/wiki/ckeditor/plugins/ExoWikiPage.vue
+++ wiki-webapp/src/main/webapp/javascript/eXo/wiki/ckeditor/plugins/ExoWikiPage.vue
@@ -23,6 +23,7 @@ export default {
   methods: {
     getPageContent() {
       if (this.pageName) {
+        this.pageName = this.pageName.replace(/[&/\\#[\]+%@|'`"^:;*?=<>{}]+/g, '_');
         const self = this;
         let url = `/${eXo.env.portal.containerName}/${eXo.env.portal.rest}/wiki`;
         if (eXo.env.portal.spaceId) {
