8446f8d54a98ad0d047ce4ab5d97b3bd5ff5cdb8
Helmi Akermi <hakermi@exoplatform.com>
2021-02-10 15:13:33 +0100

    MAINT-38133 : fix wiki page with images export pdf issue
    update
    
    update

--- wiki-webui/src/main/java/org/exoplatform/wiki/webui/control/action/ExportAsPDFActionComponent.java
+++ wiki-webui/src/main/java/org/exoplatform/wiki/webui/control/action/ExportAsPDFActionComponent.java
@@ -1,6 +1,8 @@
 package org.exoplatform.wiki.webui.control.action;
 
 import com.lowagie.text.pdf.BaseFont;
+import org.apache.commons.codec.binary.Base64;
+import org.exoplatform.commons.utils.CommonsUtils;
 import org.exoplatform.container.ExoContainerContext;
 import org.exoplatform.download.DownloadService;
 import org.exoplatform.download.InputStreamDownloadResource;
@@ -12,13 +14,18 @@ import org.exoplatform.webui.config.annotation.EventConfig;
 import org.exoplatform.webui.event.Event;
 import org.exoplatform.webui.ext.filter.UIExtensionFilter;
 import org.exoplatform.webui.ext.filter.UIExtensionFilters;
+import org.exoplatform.wiki.WikiException;
 import org.exoplatform.wiki.commons.Utils;
+import org.exoplatform.wiki.mow.api.Attachment;
 import org.exoplatform.wiki.mow.api.Page;
-import org.exoplatform.wiki.webui.UIWikiPortlet;
+import org.exoplatform.wiki.service.WikiService;
 import org.exoplatform.wiki.webui.control.action.core.AbstractEventActionComponent;
 import org.exoplatform.wiki.webui.control.filter.IsUserFilter;
 import org.exoplatform.wiki.webui.control.filter.IsViewModeFilter;
 import org.exoplatform.wiki.webui.control.listener.MoreContainerActionListener;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.w3c.dom.Document;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 import org.xhtmlrenderer.resource.FSEntityResolver;
@@ -70,10 +77,9 @@ public class ExportAsPDFActionComponent extends AbstractEventActionComponent {
       dataIn.close();
       String css = "<head><style type=\"text/css\"> " + stringBuilder.toString() + " </style></head>";
       String title = currentPage.getTitle();
-
-      String content = "<h1>" + title +"</h1><hr />" + currentPage.getContent();
+      String content = "<h1>" + title +"</h1><hr />" + htmlToxhtmlAndBase64Images(currentPage);
       String encoding = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-      content = encoding + "<!DOCTYPE xsl:stylesheet [<!ENTITY nbsp \"&#160;\">]><html>" + css + "<body>" + content + "</body></html>"; 	
+      content = encoding + "<!DOCTYPE xsl:stylesheet [<!ENTITY nbsp \"&#160;\">]><html>" + css + "<body>" + content + "</body></html>";
       
       File pdfFile = createPDFFile(title, content);
       DownloadService dservice = (DownloadService) ExoContainerContext.getCurrentContainer()
@@ -83,7 +89,7 @@ public class ExportAsPDFActionComponent extends AbstractEventActionComponent {
 							"applications/vnd.pdf, text/pdf, text/x-pdf");
       dresource.setDownloadName(title + ".pdf") ;
       String downloadLink = dservice.getDownloadLink(dservice.addDownloadResource(dresource)) ;
-      
+
       RequireJS requireJS = event.getRequestContext().getJavascriptManager().getRequireJS();      
       requireJS.require("SHARED/UIWikiPortlet", "UIWikiPortlet").addScripts("UIWikiPortlet.ajaxRedirect('" + downloadLink + "');");
       super.processEvent(event);      
@@ -108,7 +114,9 @@ public class ExportAsPDFActionComponent extends AbstractEventActionComponent {
         DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
         builder.setEntityResolver(FSEntityResolver.instance());
         Document document = builder.parse(new ByteArrayInputStream(content.toString().getBytes()));
-        renderer.setDocument(document, null);
+        renderer.getSharedContext().setReplacedElementFactory(new CustomImageReplacedElementFactory(renderer.getSharedContext().getReplacedElementFactory()));
+        renderer.getSharedContext().getTextRenderer().setSmoothingThreshold(0);
+        renderer.setDocument(document, CommonsUtils.getCurrentDomain());
         renderer.layout();
         renderer.createPDF(os);
       } catch (Exception e) {
@@ -120,5 +128,25 @@ public class ExportAsPDFActionComponent extends AbstractEventActionComponent {
       }
       return pdfFile;
     }
+    private String htmlToxhtmlAndBase64Images(Page page) throws WikiException {
+      WikiService wikiService = CommonsUtils.getService(WikiService.class);
+      org.jsoup.nodes.Document doc = Jsoup.parse(page.getContent());
+      doc.outputSettings().syntax(org.jsoup.nodes.Document.OutputSettings.Syntax.xml);
+      Elements elements = doc.getAllElements();
+      for(Element element : elements){
+        if(element.tagName().equals("img")){
+          String src = element.attr("src");
+          String imgName = src.substring(src.lastIndexOf("/") + 1);
+          Attachment imageAttachment = wikiService.getAttachmentOfPageByName(imgName,page,true);
+          if(imageAttachment.getMimeType().startsWith("image")){
+            byte[] bytes = imageAttachment.getContent();
+            element.attr("src", "base64," + Base64.encodeBase64String(bytes));
+            element.attr("style","width:100%;height:100%");
+          }
+        }
+      }
+      return doc.html();
+    }
   }
+
 }
